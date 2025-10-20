1f01f50a1d36860cf2f1e0c5610578a3e386e650
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2025-10-20 10:29:57 +0300

    add files

--- 10_3main.cpp
+++ 10_3main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <iomanip>
 
-int main() {
+int main()
+{
     const int columns = 8;
     const int rows = 16;
     int ascii;
     char symbol;
 
-    std::cout << "Columns and rows of ASCII:\n\n";
-
-    // Верхній рядок із номерами стовпців
-    std::cout << "   ";
+    std::cout << "Tablet of ASCII: " << std::endl;
+    std::cout << "    ";
     for (int i = 0; i < columns; ++i)
         std::cout << std::setw(10) << i;
     std::cout << std::endl;
-
-    // Основна таблиця
-    for (int j = 0; j < rows; ++j) {
-        std::cout << std::setw(2) << j << " ";  // номер рядка зліва
-        for (int i = 0; i < columns; ++i) {
+    for (int j = 0; j < rows; ++j)
+    {
+        std::cout << std::setw(2) << j << " ";
+        for (int i = 0; i < columns; ++i)
+        {
             ascii = i * rows + j;
             if (ascii >= 32 && ascii < 127)
                 symbol = static_cast<char>(ascii);
